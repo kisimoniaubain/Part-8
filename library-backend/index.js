f95d4e4530e4e0e29c7465d98ab68d530047fb90
Kisimoni Aubain <kisimoniaubain@gmail.com>
2026-08-04 13:22:37 +0300

    Exercise 8.5

--- library-backend/index.js
+++ library-backend/index.js
@@ -95,7 +95,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,11 +107,21 @@ const resolvers = {
     authorCount: () => authors.length,
 
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let filteredBooks = books
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(
+          (book) => book.author === args.author
+        )
       }
 
-      return books.filter((book) => book.author === args.author)
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(
+          (book) => book.genres.includes(args.genre)
+        )
+      }
+
+      return filteredBooks
     },
 
     allAuthors: () =>
@@ -134,4 +144,3 @@ startStandaloneServer(server, {
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
-
